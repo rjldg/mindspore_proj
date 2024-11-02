12346a3aadc9682b3538de1f571dbff4dfee1001
rjldg <rjlimdeguzman@gmail.com>
2024-11-02 22:12:01 +0800

    implemented main script with image input, auto-generated output(classification and rag text generation)

--- main.py
+++ main.py
@@ -34,17 +34,22 @@ client = boto3.client('bedrock-agent-runtime',
                       region_name = os.getenv("AI_SPEC_REGION"),
                       aws_access_key_id = os.getenv("AI_SPEC_ACCESS_KEY_ID"),
                       aws_secret_access_key = os.getenv("AI_SPEC_SECRET_ACCESS_KEY_ID"))
-class_names = {0:'algal_leaf',1:'anthracnose',2:'bird_eye_spot',3:'brown_blight',4:'gray_light',5:'healthy',6:'red_leaf_spot',7:'white_spot'}
 knowledge_base_id = os.getenv("AI_SPEC_KNOWLEDGE_BASE_ID")
 model_arn = os.getenv("AI_SPEC_MODEL_ARN")
 
+# CLASS NAMES FOR REFERENCE
+class_names = {0:'algal_leaf',1:'anthracnose',2:'bird_eye_spot',3:'brown_blight',4:'gray_light',5:'healthy',6:'red_leaf_spot',7:'white_spot'}
+ref_class_names = {'algal_leaf': 'Algal Leaf', 'anthracnose': "Anthracnose", 'bird_eye_spot': 'Bird Eye Spot', 
+                   'brown_blight': 'Brown Blight', 'gray_light': 'Gray Blight', 'healthy': 'Healthy Tea Leaf',
+                   'red_leaf_spot': 'Red Leaf Spot', 'white_spot': 'White Spot'}
+
+# FUNCTION DEFINITIONS
 def preprocess_image(image_path):
     
     image = Image.open(image_path).convert('RGB')
     image = image.resize((cfg['WIDTH'], cfg['HEIGHT']))
     image = np.array(image).astype(np.float32)
     
-    # Normalize the image
     image = (image - [cfg['_R_MEAN'], cfg['_G_MEAN'], cfg['_B_MEAN']]) / [cfg['_R_STD'], cfg['_G_STD'], cfg['_B_STD']]
     image = np.transpose(image, (2, 0, 1))
     image = np.expand_dims(image, axis=0) 
@@ -67,7 +72,6 @@ def predict(image_path):
     image = preprocess_image(image_path)
     model = load_model()
 
-    # Predict and calculate confidence level
     output = model.predict(image)
     softmax = Softmax()
     probabilities = softmax(output).asnumpy()
@@ -91,28 +95,33 @@ def retrieve_and_generate(input, knowledge_base_id, model_arn):
 
     return response
 
-def image_classifier(image_path):
+def model(image_path):
 
     predicted_class, confidence = predict(image_path)
     
-    print(f"Prediction: {predicted_class}")
-    print(f"Confidence: {confidence:.2f}")
+    # Image classifier Results
+    print(f"\n\tPrediction: {predicted_class}")
+    print(f"\tConfidence: {confidence:.2f}\n")
 
-    # Dispaly the results accordingly
     image = Image.open(image_path)
     plt.imshow(image)
     plt.title(f"Predicted: {predicted_class} ({confidence:.2f})")
     plt.axis('off')
     plt.show()
 
+    print("Generating Response...\n")
 
+    # RAG Foundation Model Text Output
+    response = retrieve_and_generate(f'List all the information about this tea sickness based from your knowledge base extensively and in great detail: {ref_class_names.get(predicted_class)}', knowledge_base_id=knowledge_base_id, model_arn=model_arn)
+    generated_response = response['output']['text']
+    print(generated_response)
 
-    
+# MAIN PROGRAM
 if __name__ == '__main__':
     print("******************************************************** TEA LEAF DISEASE CLASSIFICATION ********************************************************\n");
     image_path = input("\tEnter the filename/path of image: ")
     if os.path.exists(image_path):
-        image_classifier(image_path)
-        print("OK")
+        model(image_path)
+        print("\nResponse Generated.")
     else:
         print(f"Error: File '{image_path}' does not exist.")
